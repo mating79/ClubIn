7a1eea561457d3cf3f3e1987da18ce398f0cf25c
matin <matingivi1379@gmail.com>
2020-10-09 18:42:45 +0330

    Responsive Web Design

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+} 
